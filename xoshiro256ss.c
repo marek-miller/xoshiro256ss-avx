cdca9d2ce710790a260d4f4a15bb57d9f0ca8b15
⧉⧉⧉ <marek.miller@protonmail.com>
2024-05-11 20:08:23 +0200

    Add splitmix64 function to generate seed

--- xoshiro256ss.c
+++ xoshiro256ss.c
@@ -2,3 +2,13 @@
 #include <stdint.h>
 
 #include "xoshiro256ss.h"
+
+
+uint64_t xoshiro256ss_splitmix64(uint64_t *state) {
+	uint64_t rt = (*state += UINT64_C(0x9E3779B97f4A7C15));
+	rt = (rt ^ (rt >> 30)) * UINT64_C(0xBF58476D1CE4E5B9);
+	rt = (rt ^ (rt >> 27)) * UINT64_C(0x94D049BB133111EB);
+
+	return rt ^ (rt >> 31);
+}
+
