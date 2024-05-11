5d109c4db70d9f1a7de5f99bdcb00f66b3fe2944
⧉⧉⧉ <marek.miller@protonmail.com>
2024-05-11 12:09:17 +0200

    Reformat

--- xoshiro256starstar.h
+++ xoshiro256starstar.h
@@ -12,12 +12,15 @@ struct xoshiro256starstar_smpl {
 	_Alignas(32) uint64_t a[4];
 };
 
-int xoshiro256starstar_init(struct xoshiro256starstar *rng, uint64_t seed);
+int
+xoshiro256starstar_init(struct xoshiro256starstar *rng, uint64_t seed);
 
-size_t xoshiro256starstar_filln(struct xoshiro256starstar *rng,
+size_t
+xoshiro256starstar_filln(struct xoshiro256starstar *rng,
 	struct xoshiro256starstar_smpl *buf, size_t n);
 
-inline double u64_to_f64n(uint64_t a)
+inline double
+u64_to_f64n(uint64_t a)
 {
 	return (a >> 11) * 0x1.0p-53;
 }
