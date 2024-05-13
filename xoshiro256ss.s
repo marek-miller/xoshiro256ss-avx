fb26537ed91e4a95bd8cd5b06cc5891463669df1
⧉⧉⧉ <marek.miller@protonmail.com>
2024-05-13 07:15:04 +0200

    Make smarter use of three operand syntax

--- xoshiro256ss.s
+++ xoshiro256ss.s
@@ -17,29 +17,24 @@ xoshiro256ss_filln:
 	vmovdqa ymm3, 96[rdi]
 
 .l1:	; compute the result
-	vmovdqa ymm4, ymm1
-	vpsllq	ymm4, ymm4, 2
-	vpaddq	ymm4, ymm4, ymm1
-	vmovdqa ymm5, ymm4
-	vpsllq	ymm4, ymm4, 7
-	vpsrlq	ymm5, ymm5, 57
-	vpor	ymm4, ymm4, ymm5
-	vmovdqa	ymm5, ymm4
-	vpsllq	ymm4, ymm4, 3
-	vpaddq	ymm4, ymm4, ymm5
+	vpsllq	ymm4, ymm1, 2
+	vpaddq	ymm4, ymm1
+	vpsrlq	ymm5, ymm4, 57
+	vpsllq	ymm4, 7
+	vpor	ymm4, ymm5
+	vpsllq	ymm5, ymm4, 3
+	vpaddq	ymm4, ymm5
 	vmovdqa [rsi], ymm4
-	; update state
-	vmovdqa ymm4, ymm1
-	vpsllq	ymm4, 17
-	vpxor	ymm2, ymm2, ymm0
-	vpxor	ymm3, ymm3, ymm1
-	vpxor	ymm1, ymm1, ymm2
-	vpxor	ymm0, ymm0, ymm3
-	vpxor	ymm2, ymm2, ymm4
-	vmovdqa	ymm4, ymm3
-	vpsllq	ymm3, ymm3, 45
-	vpsrlq	ymm4, ymm4, 19
-	vpor	ymm3, ymm3, ymm4
+	; update the state
+	vpsllq	ymm4, ymm1, 17
+	vpxor	ymm2, ymm0
+	vpxor	ymm3, ymm1
+	vpxor	ymm1, ymm2
+	vpxor	ymm0, ymm3
+	vpxor	ymm2, ymm4
+	vpsrlq	ymm4, ymm3, 19
+	vpsllq	ymm3, 45
+	vpor	ymm3, ymm4
 	; rinse and repeat
 	add	rsi, 0x20	
 	dec	rdx
